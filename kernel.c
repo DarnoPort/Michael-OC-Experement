f90f5aa01160fbff0e9db71f4f8d313932b0ad3f
DarnoPort <egorzikin68@gmail.com>
2026-09-22 01:16:08 +0700

    Phase 22: update startup banner

--- kernel.c
+++ kernel.c
@@ -404,7 +404,7 @@ void kernel_main(unsigned int magic, unsigned int info_addr) {
     init_pic();
     init_pit(100);
 
-    print_string("=== Michael OS " MICHAEL_OS_VERSION_STRING ": Terminal Foundation ===\n", 0x0A);
+    print_string("=== Michael OS " MICHAEL_OS_VERSION_STRING ": Shell Interface ===\n", 0x0A);
 
     if (!memory_init(magic, info_addr)) {
         print_string("WARNING: physical memory manager initialization failed.\n", 0x0C);
