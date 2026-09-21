4d91768ff26f362d2eec4e23e2b18ee7c133d3b9
DarnoPort <egorzikin68@gmail.com>
2026-09-21 22:21:07 +0700

    Phase 16: generalize ELF loader interface

--- elf.h
+++ elf.h
@@ -3,6 +3,11 @@
 
 #include "process.h"
 
+int elf_load_user_process_from_image(
+    struct process* process,
+    const unsigned char* image,
+    unsigned int image_size
+);
 int elf_load_user_process(struct process* process);
 
 #endif
