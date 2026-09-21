216e67940a413a0627a918283301f9a3512a42e3
DarnoPort <egorzikin68@gmail.com>
2026-09-21 18:06:22 +0700

    Fix protected-mode setup and install a private GDT

--- boot.asm
+++ boot.asm
@@ -1,4 +1,7 @@
-; Константы для заголовка Multiboot
+; NanoOS Multiboot entry point.
+; GRUB enters the kernel in 32-bit protected mode, but we install
+; our own flat GDT instead of relying on GRUB's temporary GDT.
+
 MBALIGN  equ  1 << 0
 MEMINFO  equ  1 << 1
 FLAGS    equ  MBALIGN | MEMINFO
@@ -14,7 +17,7 @@ align 4
 section .bss
 align 16
 stack_bottom:
-    resb 16384 ; Выделяем 16 КБ для стека
+    resb 16384
 stack_top:
 
 section .text
@@ -22,12 +25,36 @@ global _start
 extern kernel_main
 
 _start:
-    ; Устанавливаем стек
-    mov esp, stack_top
-    ; Вызываем функцию main из нашего кода на Си
-    call kernel_main
-    ; Если Си-код завершится, останавливаем процессор
     cli
+
+    lgdt [gdt_descriptor]
+    jmp 0x08:.gdt_loaded
+
+.gdt_loaded:
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    mov esp, stack_top
+    xor ebp, ebp
+
+    call kernel_main
+
 .hang:
+    cli
     hlt
     jmp .hang
+
+align 8
+gdt_start:
+    dq 0x0000000000000000
+    dq 0x00CF9A000000FFFF
+    dq 0x00CF92000000FFFF
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
