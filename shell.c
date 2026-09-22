e48fa017e511d2ad8cc33c16f434438c980e2e22
DarnoPort <egorzikin68@gmail.com>
2026-09-22 20:40:14 +0700

    Fix stdio test output newline

--- shell.c
+++ shell.c
@@ -2750,7 +2750,7 @@ static int command_install_stdio_test(void) {
     );
     print_uint(image_size, 0x0F);
     print_string(
-        " bytes).\\n",
+        " bytes).\n",
         0x0A
     );
     return 1;
